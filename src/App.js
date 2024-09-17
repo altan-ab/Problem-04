36fc7b0462b5d813225e6ccf6ec392a4d81f78e9
altan-ab <altan.abduloglu@gmail.com>
2024-09-17 13:49:40 +0300

    initial-setup-refactor

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ export default function Navbar() {
   const [isAuthUser, setIsAuthUser] = useState(false)
 
   return (
-    <header className="navbar-wrapper" style={{ margin: '10px 5%' }}>
-      <nav className="navbar">
+    <header className="navbar-wrapper">
+      <nav className="navbar flex justify-center items-center flex-col">
         <Logo />
-        <div className="navbar-links">
+        <div className="navbar-links mt-4">
           {isAuthUser ? (
             <>
-              <a href="#" className="navbar-link">
+              <a
+                href="#"
+                className="navbar-link block bg-purple text-white py-2 px-4 rounded-lg mb-2 text-center"
+              >
                 Dashboard
               </a>
               <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
               <a
                 href="#"
-                className="navbar-link"
+                className="navbar-link block bg-purple text-white py-2 px-4 rounded-lg text-center"
                 onClick={() => setIsAuthUser(false)}
               >
                 Sign out
@@ -27,9 +30,10 @@ export default function Navbar() {
             </>
           ) : (
             <>
+              <h1 className="mb-8 text-black">Hesabınıza Giriş Yapın</h1>
               <a
                 href="#"
-                className="navbar-link"
+                className="navbar-link block bg-purple text-white py-2 px-4 rounded-lg text-center"
                 onClick={() => setIsAuthUser(true)}
               >
                 Login
@@ -44,7 +48,7 @@ export default function Navbar() {
 
 function Logo() {
   return (
-    <div className="flex lg:flex-1">
+    <div className="flex lg:flex-1 mt-10 mb-3">
       <a href="#" className="-m-1.5 p-1.5">
         <Image
           className="h-8 w-auto"
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      purple: '#818cf8',
+      white: '#f8fafc',
+    },
   },
   plugins: [],
 }
